bbda6ea9a48d2ce83daa78a5dbe428f56663ce29
AGRIM ASWAL <124095607+agrimaswal@users.noreply.github.com>
2023-07-14 02:18:03 +0530

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -1,32 +1,35 @@
-import java.util.HashMap;
-import java.util.Map;
-
 
 
 class twosum {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            if (numMap.containsKey(complement)) {
-                return new int[] { numMap.get(complement), i };
+        int [] result=new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+
+                    
+                }
+                
+
             }
-            
-            numMap.put(nums[i], i);
         }
-        
-        throw new IllegalArgumentException("No two numbers add up to the target");
-    }
-    public static void main(String[] args) {
-    int[] nums = {2, 7, 11, 15};
-    int target = 9;
-    
-    twosum solution = new twosum();
-    int[] result = solution.twoSum(nums, target);
-    
-    System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
-}
+        return new int[0];
 
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        twosum s=new twosum();
+        int[] k=s.twoSum(nums, target);
+        System.out.println("["+k[0]+","+k[1]+"]");
+    }
 }
